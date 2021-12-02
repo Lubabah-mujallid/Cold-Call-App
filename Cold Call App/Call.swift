f3968ec6ae8f8f25803abaed1858159537dae875
Lubabah-mujallid <90623112+Lubabah-mujallid@users.noreply.github.com>
2021-12-02 12:30:27 +0000

    ++

---
+++ Cold Call App/Call.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+class Call {
+    var currentCaller = 0
+    static let people = ["lubabah", "kida", "rudy", "stros", "brad", "pro", "fine", "kitt", "gim"]
+    func nextCaller() {
+        currentCaller+=1
+    }
+}
+
--- Cold Call App/ViewController.swift
+++ Cold Call App/ViewController.swift
@@ -1,19 +1,27 @@
-//
-//  ViewController.swift
-//  Cold Call App
-//
-//  Created by admin on 02/12/2021.
-//
+//Create an application that will pick out a random name from an Array of names every time the cold call button is clicked. We will be hard coding this Array of names in our View Controller. We will be using Auto Layout in this assignment. Make sure your application has behavior similar to the gif image below:
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    let call = Call()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        if call.currentCaller < Call.people.count - 1 {
+            call.nextCaller()
+        }
+        else { call.currentCaller = 0 }
+        updateUI()
+    }
+    
+    func updateUI() {
+        label.text = Call.people[call.currentCaller]
+    }
+    
 }
 
