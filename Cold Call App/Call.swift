9ee90c93280732054f1a2a013006f15adfe6da5d
Lubabah-mujallid <90623112+Lubabah-mujallid@users.noreply.github.com>
2021-12-02 13:25:31 +0000

    random numbet

--- Cold Call App/Call.swift
+++ Cold Call App/Call.swift
@@ -1,10 +1,25 @@
 import Foundation
+import UIKit
 
 class Call {
+    var currentNumber = 0
     var currentCaller = 0
     static let people = ["lubabah", "kida", "rudy", "stros", "brad", "pro", "fine", "kitt", "gim"]
     func nextCaller() {
         currentCaller = Int.random(in: 0...Call.people.count-1)
+    }
+    func nextNumber() {
+        currentNumber = Int.random(in: 1...5)
+    }
+    func getColor() -> UIColor{
+        switch currentNumber {
+        case 1: return UIColor.red
+        case 2: return UIColor.red
+        case 3: return UIColor.orange
+        case 4: return UIColor.yellow
+        case 5: return UIColor.green
+        default: return UIColor.black
+        }
     }
 }
 
--- Cold Call App/ViewController.swift
+++ Cold Call App/ViewController.swift
@@ -1,9 +1,10 @@
-//Create an application that will pick out a random name from an Array of names every time the cold call button is clicked. We will be hard coding this Array of names in our View Controller. We will be using Auto Layout in this assignment. Make sure your application has behavior similar to the gif image below:
+//Let's add another feature to our previous application. Whenever we click on the Cold Call button, we will not only get a random name displayed on the top but also a random number from 1 to 5 inclusive in the center of the screen. If the number is 1 or 2, the number should have a red color, if the number is 3 or 4 then the number should have an orange color, and finally, if the number is 5 then the number should have a green color.
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numLabel: UILabel!
     @IBOutlet weak var label: UILabel!
     let call = Call()
     
@@ -13,11 +14,15 @@ class ViewController: UIViewController {
 
     @IBAction func button(_ sender: Any) {
         call.nextCaller()
+        call.nextNumber()
         updateUI()
     }
     
     func updateUI() {
         label.text = Call.people[call.currentCaller]
+        numLabel.text = String(call.currentNumber)
+        numLabel.textColor = call.getColor()
+        
     }
     
 }
